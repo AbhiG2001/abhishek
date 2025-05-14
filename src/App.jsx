052dcb33c69e0da9814cbb468f57291471f0900a
Abhishek Ghuge <drabhi2126@gmail.com>
2025-05-14 17:54:09 +0530

    routing

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+import Footer from './Components/Footer/Footer'
+import Header from './Components/Header/Header'
+import Home from './Components/Home/Home'
+
+function App() {
+
+  return (
+    <>
+    {/* <h1 className='bg-green-600 p-4'>React Router</h1>
+    <Header/>
+    <Home/>
+    <Footer/>   */}
+    </>
+  )
+}
+
+export default App
